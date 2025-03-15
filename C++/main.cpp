6a086b8c16256ca90d1f0a932b563be128e1ee5a
Ran-25 <amirmokhtar5678@gmail.com>
2025-03-15 10:51:58 +0800

    Add line for Authencation

--- C++/main.cpp
+++ C++/main.cpp
@@ -1,8 +1,25 @@
 #include<iostream>
 
+void Login_Account();
+
 int main(){
+    int Authencation;
+    std::cout<<"1. Log in"<<std::endl;
+    std::cout<<"2. Sign up"<<std::endl;
+    std::cin>>Authencation;
+
+    
+
+
+}
+
+void Login_Account(){
     int Account_ID, Account_PASSCODE;
 
-    std::cout<<"Enter Account ID: ";
+    std::cout<<"Enter ACCOUNT_ID: ";
     std::cin>>Account_ID;
+
+    
+    if(Account_ID )
+
 }
