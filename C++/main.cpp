cc73c5956b0aa2c48bed6eeb6d73e7c7ac031c3f
Ran-25 <amirmokhtar5678@gmail.com>
2025-03-15 12:03:39 +0800

    add some code

--- C++/main.cpp
+++ C++/main.cpp
@@ -1,6 +1,10 @@
 #include<iostream>
+#include <fstream>
+#include <string>
+
 
 void Login_Account();
+void Sign_up();
 
 int main(){
     int Authencation;
@@ -8,18 +12,38 @@ int main(){
     std::cout<<"2. Sign up"<<std::endl;
     std::cin>>Authencation;
 
-    
+    if(Authencation == 1){
+        Login_Account();
+    }
+    else if(Authencation == 2){
+        Sign_up();
+    }
+
+
 
 
 }
 
 void Login_Account(){
     int Account_ID, Account_PASSCODE;
+    std::fstream file ("Account_detail.txt");
+    std::string id;
 
     std::cout<<"Enter ACCOUNT_ID: ";
     std::cin>>Account_ID;
 
-    
-    if(Account_ID )
+    std::string Account_IDSTR = std::to_string(Account_ID);
 
+    while(file >> id){
+        if(id == Account_IDSTR){
+            std::cout << "ID exists\n";
+        }
+        else{
+            std::cout << "Account Not Exist. Need To Sign Up\n";
+        }
+    }
+}
+
+void Sign_up(){
+    std::cout<<"Nice";
 }
